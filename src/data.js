7d126348222c1dc2caaa4a4566506aadc6ce2c81
Jon Rutter <contact@jonrutter.com>
2021-11-18 20:59:11 -0500

    Test data

---
+++ src/data.js
@@ -0,0 +1,50 @@
+import InboxIcon from '@mui/icons-material/Inbox';
+import TodayIcon from '@mui/icons-material/Today';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+
+export const defaultList = [
+  {
+    name: 'Item 1',
+    description:
+      'This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. ',
+    due: '',
+    label: '',
+    id: '2c95e644-2e7a-4481-b972-d4401d5827dd',
+  },
+  {
+    name: 'Item 2',
+    description: '',
+    due: '',
+    label: '',
+    id: 'fff87c7d-ba43-4f31-a74a-0706bc632cf0',
+  },
+  {
+    name: 'Item 3',
+    description: '',
+    due: '',
+    label: '',
+    id: '0619c61b-e58b-45a0-b959-e602417994b9',
+  },
+  {
+    name: 'Item 4',
+    description: '',
+    due: '',
+    label: '',
+    id: '4e00d113-a1b3-4fdc-9d6e-36ad4f090fbc',
+  },
+  {
+    name: 'Item 5',
+    description: '',
+    due: '',
+    label: '',
+    id: '615a403a-41fd-43fc-81d2-7006442184f0',
+  },
+];
+
+export const navItems = [
+  { title: 'Inbox', list: '11', icon: <InboxIcon fontSize="small" /> },
+  { title: 'Today', list: '', icon: <TodayIcon fontSize="small" /> },
+  { title: 'Upcoming', list: '11', icon: <EventNoteIcon fontSize="small" /> },
+  { title: 'Past Due', list: '111', icon: <ScheduleIcon fontSize="small" /> },
+];
