64acd94ada4543402abdea8e5d1ef224f99fdd17
Jon Rutter <contact@jonrutter.com>
2021-11-18 20:59:42 -0500

    Drafted component

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+const Header = ({ handleDrawerToggle, mobileOpen }) => {
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="Toggle menu"
+            sx={{ mr: 2 }}
+            focusRipple
+            onClick={handleDrawerToggle}
+            sx={{ display: { md: 'none' } }}
+          >
+            {mobileOpen ? (
+              <CloseIcon sx={{ display: { xs: 'block', md: 'none' } }} />
+            ) : (
+              <MenuIcon sx={{ display: { xs: 'block', md: 'none' } }} />
+            )}
+          </IconButton>
+          <Typography variant="h6" component="h1" noWrap>
+            To Do List
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ marginTop: '5rem' }} />
+    </>
+  );
+};
+
+export default Header;
