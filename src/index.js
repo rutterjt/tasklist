6fd896fad3b22461765884da77e2ad22bd6d4285
Jon Rutter <contact@jonrutter.com>
2021-11-19 20:17:14 -0500

    Implement store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
+
+// store
+import { StoreProvider } from './store/context';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,8 +13,10 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <StoreProvider>
+      <CssBaseline />
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
