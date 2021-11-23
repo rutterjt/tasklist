9f736eb28ec81af07015a9587b958a4af2da7f47
Jon Rutter <contact@jonrutter.com>
2021-11-22 21:30:26 -0500

    Connect to store

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
+// mui
 import {
   ListItem,
   ListItemText,
@@ -9,22 +10,48 @@ import {
   Checkbox,
 } from '@mui/material';
 
-const ToDoItem = ({
-  name,
-  description,
-  due,
-  label,
-  deleteItem,
-  updateItem,
-}) => {
-  const [active, setActive] = useState(true);
-  const handleCheck = () => {
-    setActive((prev) => !prev);
+// store
+import { useStore } from '../store/context';
+import { DELETE_ITEM } from '../store/actions';
+
+const ToDoItem = ({ name, description, due, label, id }) => {
+  const { dispatch } = useStore();
+  // state to control checkbox
+  const [checked, setChecked] = useState(false);
+
+  // action creator
+  const deleteCreator = (id) => {
+    return { type: DELETE_ITEM, payload: id };
   };
+
+  // uses useCallback to prevent triggering component rerenders, since deleteItem needs to be in the useEffect dependency array
+  const deleteItem = useCallback(
+    (id) => {
+      dispatch(deleteCreator(id));
+    },
+    [dispatch]
+  );
+
+  // creates a 500ms delay before the item is deleted
+  useEffect(() => {
+    let deleteTimeout;
+    if (checked) {
+      deleteTimeout = setTimeout(() => deleteItem(id), 500);
+    } else {
+      clearTimeout(deleteTimeout);
+    }
+    return () => clearTimeout(deleteTimeout);
+  }, [checked, deleteItem, id]);
+
+  // check handler
+  const handleCheck = (e) => {
+    setChecked((prev) => !prev);
+  };
+
+  // renders only the beginning of the item's description if over 100 characters
   const displayDescription =
     description.length > 100 ? description.slice(0, 98) + '...' : description;
 
-  // timeout after checking box, before item disappears
   return (
     <>
       <ListItem disablePadding>
@@ -32,7 +59,7 @@ const ToDoItem = ({
           <Checkbox
             edge="end"
             onChange={handleCheck}
-            checked={!active}
+            checked={checked}
             inputProps={{ 'aria-labelledby': 'Delete item' }}
           />
         </ListItemIcon>
