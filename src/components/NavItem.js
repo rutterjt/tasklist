b627aeb606e3de7669f74a0ea278e9c5f071c220
Jon Rutter <contact@jonrutter.com>
2021-11-22 21:28:39 -0500

    Created component

---
+++ src/components/NavItem.js
@@ -0,0 +1,40 @@
+import React, { useMemo } from 'react';
+
+// routing
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+
+// mui
+import { ListItem, ListItemIcon, Badge, ListItemText } from '@mui/material';
+
+const NavItem = ({ title, list, icon, to }) => {
+  const { pathname } = useLocation();
+
+  const renderLink = useMemo(
+    () =>
+      React.forwardRef(function Link(itemProps, ref) {
+        return <RouterLink to={to} ref={ref} {...itemProps} role={undefined} />;
+      }),
+    [to]
+  );
+
+  return (
+    <ListItem
+      selected={pathname === to}
+      button
+      disableRipple
+      component={renderLink}
+    >
+      <ListItemIcon>
+        <Badge
+          badgeContent={list.length}
+          color={title === 'Past Due' ? 'error' : 'secondary'}
+        >
+          {icon}
+        </Badge>
+      </ListItemIcon>
+      <ListItemText primary={title} />
+    </ListItem>
+  );
+};
+
+export default NavItem;
