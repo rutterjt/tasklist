db8cf674c5c726951d99fbe543ae453c794b08f3
Jon Rutter <contact@jonrutter.com>
2021-11-22 21:31:34 -0500

    AddItem button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+// mui
 import { Box } from '@mui/material';
+
+// components
 import ToDoList from '../components/ToDoList';
+import AddItemForm from '../components/AddItemForm';
 
 const Home = ({ list }) => {
   return (
     <Box sx={{ ml: { md: '2rem' } }}>
       <ToDoList list={list} />
+      <AddItemForm />
     </Box>
   );
 };
