ff2c610a407000a1df8aa21207c970c39ce67f47
Jon Rutter <contact@jonrutter.com>
2021-11-22 21:31:19 -0500

    Completed page

---
+++ src/pages/Completed.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// mui
+import { Box, Button, List, ListItem } from '@mui/material';
+import { Delete } from '@mui/icons-material';
+
+// components
+import ToDoList from '../components/ToDoList';
+
+// store
+import { useStore } from '../store/context';
+import { EMPTY_TRASH } from '../store/actions';
+
+const Completed = ({ list }) => {
+  const { dispatch } = useStore();
+  const deleteCreator = () => ({ type: EMPTY_TRASH });
+  const deleteAll = () => dispatch(deleteCreator());
+
+  return (
+    <Box sx={{ ml: { md: '2rem' } }}>
+      <ToDoList list={list} noCheckbox />
+      <Button
+        onClick={deleteAll}
+        color="error"
+        startIcon={<Delete />}
+        sx={{ mt: '0.5rem', ml: '4rem' }}
+      >
+        Delete All
+      </Button>
+    </Box>
+  );
+};
+
+export default Completed;
