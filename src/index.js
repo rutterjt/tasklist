25bf7cc9cc454907575395020e1d25b110cb8980
Jon Rutter <contact@jonrutter.com>
2021-12-03 16:53:21 -0500

    Add LocalizationProvider

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 // store
 import { StoreProvider } from './store/context';
 
+// mui/date
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+// fonts
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -13,10 +18,12 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider>
-      <CssBaseline />
-      <App />
-    </StoreProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StoreProvider>
+        <CssBaseline />
+        <App />
+      </StoreProvider>
+    </LocalizationProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
