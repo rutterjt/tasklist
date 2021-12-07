4b18be346df1e93b9c9f291b2544a79f91694953
Jon Rutter <contact@jonrutter.com>
2021-12-07 02:31:42 -0500

    Extract form controls to own components

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -1,43 +1,17 @@
 import React, { useState } from 'react';
 
 // mui
-import {
-  Box,
-  Button,
-  Paper,
-  TextField,
-  IconButton,
-  Popover,
-  Typography,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Grid,
-  Divider,
-} from '@mui/material';
+import { Box, Button, Paper, Typography, Grid } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-
-// mui icons
-import DatePicker from '@mui/lab/DatePicker';
-import TodayIcon from '@mui/icons-material/Today';
-import UpcomingIcon from '@mui/icons-material/Upcoming';
-import InsertInvitationIcon from '@mui/icons-material/InsertInvitation';
-import DoNotDisturbAltIcon from '@mui/icons-material/DoNotDisturbAlt';
 
 // store
 import { useStore } from '../store/context';
 import { ADD_ITEM } from '../store/actions';
 
-// date
-import isToday from 'date-fns/isToday';
-import isTomorrow from 'date-fns/isTomorrow';
-import format from 'date-fns/format';
-import add from 'date-fns/add';
-
 // components
-import PriorityIcon from './PriorityIcon';
+import PriorityControl from './PriorityControl';
+import DueDateControl from './DueDateControl';
+import TextControl from './TextControl';
 
 // Add task button: controls whether the form is visible
 const AddButton = ({ setFormOpen }) => (
@@ -50,183 +24,6 @@ const AddButton = ({ setFormOpen }) => (
     Add task
   </Button>
 );
-
-// Renders controls for the task's priority
-const PriorityControl = ({ priority, setPriority }) => {
-  const [anchor, setAnchor] = useState(null);
-
-  const handleClick = (e) => {
-    setAnchor(e.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchor(null);
-  };
-
-  const handleListClick = (num) => {
-    setPriority(num);
-    handleClose();
-  };
-
-  const open = !!anchor;
-  const id = open ? 'priority-popup' : undefined;
-  return (
-    <Box>
-      <IconButton aria-label="Set Priority" onClick={handleClick}>
-        <PriorityIcon priority={priority} />
-      </IconButton>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchor}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-      >
-        <List>
-          {[1, 2, 3, 4].map((num) => (
-            <ListItem key={num}>
-              <ListItemButton onClick={() => handleListClick(num)}>
-                <ListItemIcon>
-                  <PriorityIcon priority={num} />
-                </ListItemIcon>
-                <ListItemText>Priority {num}</ListItemText>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Popover>
-    </Box>
-  );
-};
-
-const DateListItem = ({ title, icon, onClick }) => (
-  <ListItem>
-    <ListItemButton onClick={onClick}>
-      <ListItemIcon>{icon}</ListItemIcon>
-      <ListItemText>{title}</ListItemText>
-    </ListItemButton>
-  </ListItem>
-);
-
-// renders the controls for the task's due date
-const DateControl = ({ date, setDate }) => {
-  const [anchor, setAnchor] = useState(null);
-
-  const handleClick = (e) => {
-    setAnchor(e.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchor(null);
-  };
-
-  const handleDateChange = (newDate) => {
-    setDate(newDate);
-  };
-
-  const open = !!anchor;
-  const id = open ? 'priority-popup' : undefined;
-  return (
-    <Box>
-      <Button onClick={handleClick} variant="outlined">
-        {date
-          ? isToday(date)
-            ? 'Today'
-            : isTomorrow(date)
-            ? 'Tomorrow'
-            : format(date, 'MM/dd/yyyy')
-          : 'Schedule'}
-      </Button>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchor}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-      >
-        <Box sx={{ pt: '1rem', pb: '1rem' }}>
-          <DatePicker
-            label="Due Date"
-            value={date}
-            onChange={(newValue) => {
-              handleDateChange(newValue);
-            }}
-            renderInput={(params) => (
-              <TextField size="small" sx={{ m: '1rem' }} {...params} />
-            )}
-            minDate={Date.now()}
-          />
-          <List dense>
-            <Divider />
-            <DateListItem
-              title="Today"
-              icon={<TodayIcon color="success" />}
-              onClick={() => handleDateChange(new Date())}
-            />
-            <DateListItem
-              title="Tomorrow"
-              icon={<UpcomingIcon color="warning" />}
-              onClick={() => handleDateChange(add(new Date(), { days: 1 }))}
-            />
-            <DateListItem
-              title="Next Week"
-              icon={<InsertInvitationIcon color="secondary" />}
-              onClick={() => handleDateChange(add(new Date(), { weeks: 1 }))}
-            />
-            <Divider />
-            <DateListItem
-              title="No Date"
-              icon={<DoNotDisturbAltIcon />}
-              onClick={() => handleDateChange(null)}
-            />
-          </List>
-        </Box>
-      </Popover>
-    </Box>
-  );
-};
-
-// renders a text field
-const TextControl = ({
-  name,
-  value,
-  onChange,
-  required,
-  autoFocus,
-  lines = 1,
-}) => {
-  if (lines > 1) {
-    return (
-      <TextField
-        id={name}
-        label={name}
-        value={value}
-        onChange={onChange}
-        required={required}
-        autoFocus={autoFocus}
-        fullWidth
-        variant="outlined"
-        multiline
-        minRows={lines}
-        sx={{ mb: '1rem' }}
-      />
-    );
-  } else {
-    return (
-      <TextField
-        id={name}
-        label={name}
-        value={value}
-        onChange={onChange}
-        required={required}
-        autoFocus={autoFocus}
-        fullWidth
-        variant="outlined"
-        sx={{ mb: '1rem' }}
-      />
-    );
-  }
-};
 
 // renders the task creation form's ui
 const FormDisplay = ({
@@ -264,7 +61,7 @@ const FormDisplay = ({
         />
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item>
-            <DateControl date={date} setDate={setDate} />
+            <DueDateControl date={date} setDate={setDate} />
           </Grid>
           <Grid item>
             <PriorityControl priority={priority} setPriority={setPriority} />
