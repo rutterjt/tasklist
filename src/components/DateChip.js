c9803a557e7300706586931e7ef678da2cc63bac
Jon Rutter <contact@jonrutter.com>
2021-12-11 19:39:58 -0500

    DateChip component

---
+++ src/components/DateChip.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// components
+import ShortChip from './ShortChip';
+
+// date
+import { displayDate } from '../utils/date';
+
+// accepts a date value in milliseconds, unix time
+const DateChip = ({ date, noLabel = 'Unscheduled' }) => {
+  const label = displayDate(date, noLabel);
+  let color = '';
+  switch (label) {
+    case 'Today':
+      color = 'success';
+      break;
+    case 'Tomorrow':
+      color = 'warning';
+      break;
+    case 'Past Due':
+      color = 'error';
+      break;
+    default:
+      return <ShortChip label={label} variant="outlined" />;
+  }
+
+  return <ShortChip color={color} label={label} variant="outlined" />;
+};
+
+export default DateChip;
