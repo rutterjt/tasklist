a146d6799a583b3c81274ce594a71db5cb014d8c
Jon Rutter <contact@jonrutter.com>
2021-12-11 19:40:26 -0500

    Refactor to avoid nested ternary operators

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,12 +2,13 @@ import isToday from 'date-fns/isToday';
 import isTomorrow from 'date-fns/isTomorrow';
 import format from 'date-fns/format';
 
+import { isInPast } from './time';
+
 export const displayDate = (date, noDate = 'Schedule') => {
-  return date
-    ? isToday(date)
-      ? 'Today'
-      : isTomorrow(date)
-      ? 'Tomorrow'
-      : format(date, 'MM/dd/yyyy')
-    : noDate;
+  // Takes a date in milliseconds unix time, and an optional fallback string. Returns a string containing a formatted version of the date.
+  if (!date || typeof date !== 'number') return noDate;
+  else if (isToday(date)) return 'Today';
+  else if (isTomorrow(date)) return 'Tomorrow';
+  else if (isInPast(date)) return 'Past Due';
+  return format(date, 'MM/dd/yyyy');
 };
