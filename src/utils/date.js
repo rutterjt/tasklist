c99119ceef9c7a466cd5e4d73e8d441fa52ac14a
Jon Rutter <contact@jonrutter.com>
2021-12-16 19:26:17 -0500

    Fix #2

--- src/utils/date.js
+++ src/utils/date.js
@@ -4,11 +4,18 @@ import format from 'date-fns/format';
 
 import { isInPast } from './time';
 
-export const displayDate = (date, noDate = 'Schedule') => {
+export const displayDate = (
+  date,
+  noDate = 'Schedule',
+  pastDue = 'Past Due',
+  formatPastDue = false
+) => {
   // Takes a date in milliseconds unix time, and an optional fallback string. Returns a string containing a formatted version of the date.
   if (!date || typeof date !== 'number') return noDate;
   else if (isToday(date)) return 'Today';
   else if (isTomorrow(date)) return 'Tomorrow';
-  else if (isInPast(date)) return 'Past Due';
+  else if (isInPast(date)) {
+    return formatPastDue ? format(date, 'MM/dd/yyyy') : pastDue;
+  }
   return format(date, 'MM/dd/yyyy');
 };
