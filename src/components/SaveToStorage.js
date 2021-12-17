d8e745aabc74a99d94257cd436ff5680ffeab054
Jon Rutter <contact@jonrutter.com>
2021-12-17 17:16:00 -0500

    Close #4

--- src/components/SaveToStorage.js
+++ src/components/SaveToStorage.js
@@ -5,13 +5,14 @@ const setItem = (name, value) =>
   localStorage.setItem(name, JSON.stringify(value));
 
 const SaveToStorage = () => {
-  const { list, labels, deleted } = useStore();
+  const { list, labels, deleted, sortBy } = useStore();
 
   useEffect(() => {
     setItem('list', list);
     setItem('labels', labels);
     setItem('deleted', deleted);
-  }, [list, labels, deleted]);
+    setItem('sortBy', sortBy);
+  }, [list, labels, deleted, sortBy]);
 
   return null;
 };
