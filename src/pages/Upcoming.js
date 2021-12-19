00460ea94b0dcc8b202ab0c00e4660a2f67b3226
Jon Rutter <contact@jonrutter.com>
2021-12-19 12:34:27 -0500

    Create Upcoming page

---
+++ src/pages/Upcoming.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// react helmet
+import { Helmet } from 'react-helmet-async';
+
+// components
+import Layout from '../components/Layout';
+import TaskList from '../components/TaskList';
+import TaskCreateForm from '../components/TaskCreateForm';
+
+// store
+import { useStore } from '../store/useStore';
+
+// utils
+import { isDueInFuture } from '../utils/time';
+
+const Upcoming = () => {
+  const { list } = useStore();
+
+  const filteredList = list.filter(isDueInFuture);
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Upcoming | To Do List</title>
+      </Helmet>
+      <TaskList label={'Upcoming'} list={filteredList} />
+      <TaskCreateForm />
+    </Layout>
+  );
+};
+
+export default Upcoming;
