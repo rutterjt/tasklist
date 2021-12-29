a7485e8aabfeb86d585cdf8688cf6cd134745f64
Jon Rutter <contact@jonrutter.com>
2021-12-29 18:05:47 -0500

    Fix documentation

--- src/components/CustomDialog.js
+++ src/components/CustomDialog.js
@@ -5,8 +5,8 @@ import { Dialog } from '@mui/material';
 
 /**
  * Renders a dialog box.
- * @prop {boolean} props.open - If true, the dialog renders. If false, it does not.
- * @prop {function} props.onClose - Callback function that runs when the user attempts to close the dialog.
+ * @param {boolean} props.open - If true, the dialog renders. If false, it does not.
+ * @param {function} props.onClose - Callback function that runs when the user attempts to close the dialog.
  * @param {any} props.children - The dialog contents.
  * @return A dialog box.
  */
