8b2380a96501fda9fc2f6e514d86a87f3d64765f
Jon Rutter <contact@jonrutter.com>
2021-12-29 19:47:34 -0500

    Document usePopup

--- src/hooks/usePopup.js
+++ src/hooks/usePopup.js
@@ -1,5 +1,20 @@
 import { useState } from 'react';
 
+/**
+ * Manages state to open and close a modal popup.
+ *
+ * Returns an array:
+ *
+ * popupOpen: boolean value, whether popup is currently open
+ *
+ * open: function to close the popup. Called without arguments.
+ *
+ * close: function to close the popup. Called without arguments.
+ *
+ * tryClose: function to attempt to close the popup. Accepts three callbacks. If first callback returns true, the popup will be closed, and the second callback will run. If not, the popup will not close, and the third callback will run.
+ *
+ * @param {boolean} [defaultValue=false] - Default value of whether the modal popup should be open.
+ */
 export const usePopup = (defaultValue = false) => {
   const [popupOpen, setPopupOpen] = useState(defaultValue);
 
