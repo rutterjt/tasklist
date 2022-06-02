61be670e19fc1cd88d01081c1d94a03e30c31ed2
Jon Rutter <contact@jonrutter.io>
2022-06-02 19:39:59 -0400

    Convert PriorityIcon to TypeScript

--- src/components/PriorityIcon.tsx
+++ src/components/PriorityIcon.tsx
@@ -1,33 +1,27 @@
 import React from 'react';
 
-// proptypes
-import PropTypes from 'prop-types';
-
 // mui
 import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
 import FlagIcon from '@mui/icons-material/Flag';
 import { indigo, purple, red } from '@mui/material/colors';
+import { PriorityType } from '../types';
 
 const flagColors = [null, red[500], purple[500], indigo[500], indigo[500]];
+
+type Props = {
+  priority: PriorityType;
+};
 
 /**
  * Renders a flag icon to display a priority value. The icon's color changes based on the priority.
  * @param {number} priority - The priority.
  */
-const PriorityIcon = ({ priority }) => {
+export const PriorityIcon: React.FC<Props> = ({ priority }) => {
   if (priority >= 4) {
     return <FlagOutlinedIcon sx={{ display: 'block' }} />;
   } else {
     return <FlagIcon sx={{ display: 'block', color: flagColors[priority] }} />;
   }
-};
-
-PriorityIcon.defaultProps = {
-  priority: 4,
-};
-
-PriorityIcon.propTypes = {
-  priority: PropTypes.number.isRequired,
 };
 
 export default PriorityIcon;
