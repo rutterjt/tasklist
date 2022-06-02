cf95fd4a236091497f7981d9cad288ef3f955958
Jon Rutter <contact@jonrutter.io>
2022-06-02 19:40:11 -0400

    Convert SaveToStorage to TypeScript

--- src/components/SaveToStorage.tsx
+++ src/components/SaveToStorage.tsx
@@ -3,21 +3,21 @@ import { useEffect } from 'react';
 // store
 import { useStore } from '../store/useStore';
 
-const setItem = (name, value) =>
+const setItem = (name: string, value: any) =>
   localStorage.setItem(name, JSON.stringify(value));
 
 /**
  * Utility component. Listens for state changes, and updates localStorage with the new state whenever state changes.
  */
 const SaveToStorage = () => {
-  const { list, labels, deleted, sortBy } = useStore();
+  const { list, labels, deleted } = useStore();
 
   useEffect(() => {
     setItem('list', list);
     setItem('labels', labels);
     setItem('deleted', deleted);
-    setItem('sortBy', sortBy);
-  }, [list, labels, deleted, sortBy]);
+    // setItem('sortBy', sortBy);
+  }, [list, labels, deleted]);
 
   return null;
 };
