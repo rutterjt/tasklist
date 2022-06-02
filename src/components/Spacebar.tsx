1a2c930964823b23510ba766b33c6f5a28097699
Jon Rutter <contact@jonrutter.io>
2022-06-02 19:40:35 -0400

    Convert Spacebar to TypeScript

--- src/components/Spacebar.tsx
+++ src/components/Spacebar.tsx
@@ -6,13 +6,11 @@ import { Toolbar } from '@mui/material';
 /**
  * Creates spacing under the main site header.
  */
-const Spacebar = () => {
-  return (
-    <>
-      <Toolbar />
-      <div style={{ marginTop: '1rem', minHeight: '1rem' }}></div>
-    </>
-  );
-};
+export const Spacebar: React.FC = () => (
+  <>
+    <Toolbar />
+    <div style={{ marginTop: '1rem', minHeight: '1rem' }}></div>
+  </>
+);
 
 export default Spacebar;
