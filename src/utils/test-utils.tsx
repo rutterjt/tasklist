8a2a939e5d9799fa5d8c2641a27b68ddc923f1e9
Jon Rutter <contact@jonrutter.io>
2022-06-04 03:12:41 -0400

    Add React Helmet provider to test wrapper

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -4,23 +4,26 @@ import { render, RenderOptions } from '@testing-library/react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { StoreProvider } from '../store/context';
+import { HelmetProvider } from 'react-helmet-async';
 
 const Wrapper: React.FC = ({ children }) => (
-  <LocalizationProvider dateAdapter={AdapterDateFns}>
-    <StoreProvider
-      testDefaultState={{
-        labels: [
-          {
-            name: 'Preloaded Test Label',
-            color: 'blue',
-            id: 'preloaded-test-label',
-          },
-        ],
-      }}
-    >
-      {children}
-    </StoreProvider>
-  </LocalizationProvider>
+  <HelmetProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StoreProvider
+        testDefaultState={{
+          labels: [
+            {
+              name: 'Preloaded Test Label',
+              color: 'blue',
+              id: 'preloaded-test-label',
+            },
+          ],
+        }}
+      >
+        {children}
+      </StoreProvider>
+    </LocalizationProvider>
+  </HelmetProvider>
 );
 
 const customRender = (
